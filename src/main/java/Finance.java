7102e23eb4dd6b5b41dc05d9547bba8779b7df44
shangyin.zou <zoushangyin@gmail.com>
2022-05-13 11:03:00 +0900

    module5

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,5 +1,3 @@
-package com.h2;
-
 import java.util.Arrays;
 import java.util.Map;
 import com.h2.BestLoanRates;
@@ -7,23 +5,23 @@ import com.h2.SavingsCalculator;
 import com.h2.MortgageCalculator;
 
 public class Finance {
-    public final static String BEST_LOAN_RATES = "bestLoanRates";
-    public final static String SAVINGS_CALCULATOR = "savingsCalculator";
-    public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
+    public static final String BEST_LOAN_RATES = "bestLoanRates";
+    public static final String SAVINGS_CALCULATOR = "savingsCalculator";
+    public static final String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
-    public final static Map<String,String> commandsToUsage = Map.of(BEST_LOAN_RATES, "usage: bestLoanRates",
+    public static final Map<String,String> commandsToUsage = Map.of(BEST_LOAN_RATES, "usage: bestLoanRates",
             SAVINGS_CALCULATOR, "usage: savingsCalculator <credits separated by ','> <debits separated by ','>",
             MORTGAGE_CALCULATOR, "usage: mortgageCalculator <loanAmount> <termInYears> <annualRate>");
 
     private static boolean validateCommandArguments(String[] args){
         switch (args[0]){
             case BEST_LOAN_RATES:
-                return args.length > 1;
+                return args.length == 1;
             case SAVINGS_CALCULATOR:
-                return args.length > 3;
+                return args.length == 3;
 
             case MORTGAGE_CALCULATOR:
-                return args.length > 4;
+                return args.length == 4;
 
         }
         return false;
@@ -50,6 +48,7 @@ public class Finance {
         String command = args[0];
         if (!commandsToUsage.containsKey(command)){
             System.out.println(command + ": command not found");
+            return;
         }
         boolean isValidCommand = validateCommandArguments(args);
         if(!isValidCommand){
