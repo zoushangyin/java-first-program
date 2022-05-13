a47b98ff6f478ac344d33dd910950e46e544d2cf
shangyin.zou <zoushangyin@gmail.com>
2022-05-13 11:20:00 +0900

    module6

--- src/main/java/com/h2/SavingsCalculator.java
+++ src/main/java/com/h2/SavingsCalculator.java
@@ -44,11 +44,11 @@ public class SavingsCalculator {
         String[] debitsAsString = args[1].split(",");
         float[] credits = new float[creditsAsString.length];
         for(int i=0; i<creditsAsString.length; i++){
-            credits[i] = Float.parseFloat(creditsAsString[i]);
+            credits[i] = Utilities.getFloatValue(creditsAsString[i]);
         }
         float[] debits = new float[debitsAsString.length];
         for(int i=0; i<debitsAsString.length; i++){
-            debits[i] = Float.parseFloat(debitsAsString[i]);
+            debits[i] = Utilities.getFloatValue(debitsAsString[i]);
         }
 
         SavingsCalculator calculator  = new SavingsCalculator(credits, debits);
